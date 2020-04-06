1f64ce46fef84040ecb5260ee4b75f8e7c422e7b
Vance Morgan <vance.morgan@outlook.com>
2020-04-06 01:28:06 -0500

    Added .NET Core console project

---
+++ src/GoProReName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoProReName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
