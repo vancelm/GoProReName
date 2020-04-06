0e17529c685b4a9121e535ca4313dfae3a66323d
Vance Morgan <vance.morgan@outlook.com>
2020-04-06 04:22:32 -0500

    Basic command like arguments added

--- src/GoProReName/Program.cs
+++ src/GoProReName/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.IO;
 
 namespace GoProReName
 {
     class Program
     {
+        class Options
+        {
+            [Option(
+                'r',
+                "recursive",
+                Required = false,
+                Default = false,
+                HelpText = "Recursively renames all files in the current directory and all subdirectories.")]
+            public bool Recursive { get; set; }
+
+            [Value(
+                0,
+                MetaName = "path",
+                Required = false,
+                HelpText = "Optional directory containing files to rename.")]
+            public string Directory { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Environment.CurrentDirectory);
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    ProcessFiles(string.IsNullOrWhiteSpace(o.Directory) ?
+                        Environment.CurrentDirectory :
+                        o.Directory, o.Recursive);
+                });
+        }
+
+        static void ProcessFiles(string directory, bool recursive)
+        {
+            var files = Directory.GetFiles(directory);
+            foreach (var f in files)
+            {
+                RenameFile(f);
+            }
+
+            if (recursive)
+            {
+                var directories = Directory.GetDirectories(directory);
+                foreach (var d in directories)
+                {
+                    ProcessFiles(d, true);
+                }
+            }
+        }
+
+        static void RenameFile(string file)
+        {
+            Console.WriteLine(file);
         }
     }
 }
